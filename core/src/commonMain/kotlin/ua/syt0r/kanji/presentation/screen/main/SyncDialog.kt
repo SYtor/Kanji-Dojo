34eee9c3dbfabdf92ff682913f38ee1c34d35002
syt0r <shuliak.yaroslav@gmail.com>
2024-12-20 18:58:31 +0200

    Updated sync dialog design

--- core/src/commonMain/kotlin/ua/syt0r/kanji/presentation/screen/main/SyncDialog.kt
+++ core/src/commonMain/kotlin/ua/syt0r/kanji/presentation/screen/main/SyncDialog.kt
@@ -1,116 +1,110 @@
 package ua.syt0r.kanji.presentation.screen.main
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRowScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.CloudDownload
+import androidx.compose.material.icons.outlined.CloudSync
+import androidx.compose.material.icons.outlined.CloudUpload
+import androidx.compose.material.icons.outlined.Download
+import androidx.compose.material.icons.outlined.SyncProblem
+import androidx.compose.material.icons.outlined.Upload
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.LocalRippleConfiguration
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RippleConfiguration
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.State
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import kotlinx.datetime.LocalDateTime
-import kotlinx.datetime.format
-import kotlinx.datetime.format.byUnicodePattern
 import ua.syt0r.kanji.core.ApiRequestIssue
 import ua.syt0r.kanji.core.sync.SyncConflictResolveStrategy
 import ua.syt0r.kanji.core.sync.SyncDataDiffType
-import ua.syt0r.kanji.presentation.common.MultiplatformDialog
+import ua.syt0r.kanji.presentation.common.ExperimentalMultiplatformDialog
 
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun SyncDialog(
     state: State<SyncDialogState>,
     cancelSync: () -> Unit,
-    resolveConflict: (SyncConflictResolveStrategy) -> Unit
+    resolveConflict: (SyncConflictResolveStrategy) -> Unit,
+    navigateToAccount: () -> Unit
 ) {
 
     val dialogContent: @Composable ColumnScope.() -> Unit
-    val dialogButtons: @Composable RowScope.() -> Unit
+    val dialogButtons: @Composable FlowRowScope.() -> Unit
 
     when (val currentState = state.value) {
         SyncDialogState.Hidden -> return
 
         SyncDialogState.Uploading -> {
             dialogContent = {
-                Column(
-                    verticalArrangement = Arrangement.spacedBy(12.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    CircularProgressIndicator()
-                    Text("Uploading...")
-                }
+                LoadingLayout(
+                    imageVector = Icons.Outlined.CloudUpload,
+                    message = "Uploading..."
+                )
             }
             dialogButtons = {
-                TextButton(
-                    onClick = cancelSync
-                ) { Text("Cancel") }
+                DialogButton(cancelSync) { Text("Cancel") }
             }
         }
 
         SyncDialogState.Downloading -> {
             dialogContent = {
-                Column(
-                    verticalArrangement = Arrangement.spacedBy(12.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    CircularProgressIndicator()
-                    Text("Downloading...")
-                }
+                LoadingLayout(
+                    imageVector = Icons.Outlined.CloudDownload,
+                    message = "Downloading..."
+                )
             }
             dialogButtons = {
-                TextButton(
-                    onClick = cancelSync
-                ) { Text("Cancel") }
+                DialogButton(cancelSync) { Text("Cancel") }
             }
         }
 
         is SyncDialogState.Conflict -> {
             dialogContent = {
-                val localDateTimeFormat = LocalDateTime.Format {
-                    byUnicodePattern("yyyy-MM-dd HH:mm")
-                }
-
-                val remoteTime = currentState.remoteDataTime
-                    ?.format(localDateTimeFormat) ?: "-"
-                val localTime = currentState.lastSyncTime
-                    ?.format(localDateTimeFormat) ?: "-"
-
                 val (title, message) = when (currentState.diffType) {
                     SyncDataDiffType.RemoteNewer -> "New Data Found" to "Data on the server is newer than your local copy"
                     SyncDataDiffType.Incompatible -> "Data Conflict" to "Both remote and local data were changed since the last sync, result can't be merged"
                     else -> error("Unexpected diff type for conflict state")
                 }
-
-                Column(
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text(
-                        text = title,
-                        modifier = Modifier.align(Alignment.CenterHorizontally),
-                        style = MaterialTheme.typography.titleSmall
-                    )
-                    Text(message)
-                }
-
+                MessageLayout(
+                    title = title,
+                    message = message,
+                    imageVector = Icons.Outlined.CloudSync
+                )
             }
             dialogButtons = {
-                TextButton(
-                    onClick = cancelSync
-                ) { Text("Cancel") }
 
                 if (currentState.diffType == SyncDataDiffType.Incompatible) {
-                    TextButton(
+                    DialogButton(
                         onClick = { resolveConflict(SyncConflictResolveStrategy.UploadLocal) }
-                    ) { Text("Upload") }
+                    ) {
+                        DialogButtonIcon(Icons.Outlined.Upload)
+                        Text("Upload")
+                    }
                 }
 
                 if (
@@ -119,73 +113,173 @@ fun SyncDialog(
                         SyncDataDiffType.Incompatible
                     )
                 ) {
-                    TextButton(
+                    DialogButton(
                         onClick = { resolveConflict(SyncConflictResolveStrategy.DownloadRemote) }
-                    ) { Text("Download") }
+                    ) {
+                        DialogButtonIcon(Icons.Outlined.Download)
+                        Text("Download")
+                    }
                 }
+                DialogButton(cancelSync) { Text("Cancel") }
             }
         }
 
         is SyncDialogState.Error.Api -> {
             if (!currentState.showDialog.value) return
+            val hideErrorDialog = { currentState.showDialog.value = false }
             dialogContent = {
                 val (title, message) = when (val issue = currentState.issue) {
                     ApiRequestIssue.NoConnection -> "No Network" to "Couldn't establish network connection"
                     ApiRequestIssue.NoSubscription -> "Subscription Expired" to "Your subscription has expired, sync will be disabled"
-                    ApiRequestIssue.NotAuthenticated -> "Account Issue" to "There's an issue with your account"
-                    is ApiRequestIssue.Other -> "Unexpected Error" to issue.throwable.message!!
+                    ApiRequestIssue.NotAuthenticated -> "Session Expired" to "Sign in to your account again to continue"
+                    is ApiRequestIssue.Other -> "Unexpected Error" to
+                            (issue.throwable.message ?: "Unknown issue")
                 }
-                Column(
-                    verticalArrangement = Arrangement.spacedBy(8.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text(
-                        text = title,
-                        style = MaterialTheme.typography.titleLarge,
-                        color = MaterialTheme.colorScheme.error
-                    )
-                    Text(
-                        message,
-                        style = MaterialTheme.typography.bodySmall,
-                        color = MaterialTheme.colorScheme.onSurfaceVariant,
-                        textAlign = TextAlign.Center
-                    )
-                }
+                MessageLayout(
+                    title = title,
+                    message = message,
+                    imageVector = Icons.Outlined.SyncProblem,
+                    titleColor = MaterialTheme.colorScheme.error
+                )
             }
             dialogButtons = {
-                TextButton(
-                    onClick = { currentState.showDialog.value = false }
-                ) { Text("Cancel") }
+                when (currentState.issue) {
+                    ApiRequestIssue.NoSubscription,
+                    ApiRequestIssue.NotAuthenticated -> {
+                        DialogButton(navigateToAccount) { Text("Account") }
+                    }
+
+                    else -> Unit
+                }
+                DialogButton(hideErrorDialog) { Text("Cancel") }
             }
         }
 
         is SyncDialogState.Error.Unsupported -> {
             if (!currentState.showDialog.value) return
+            val hideErrorDialog = { currentState.showDialog.value = false }
             dialogContent = {
-                Column {
-                    Text("Data on the server is unsupported")
-                    Text("The data on the server was created using the newer version of the application and can't be applied to the currently installed version. Update the app to retrieve your data or upload your local data to the server")
-                }
-
+                MessageLayout(
+                    title = "Data on the server is unsupported",
+                    message = "The data on the server was created using the newer version of the application and is not compatible with the currently installed version. Update the app to retrieve your data or upload your local data to the server",
+                )
             }
             dialogButtons = {
-                TextButton(
-                    onClick = { currentState.showDialog.value = false }
-                ) { Text("Cancel") }
-
-                TextButton(
+                DialogButton(
                     onClick = { resolveConflict(SyncConflictResolveStrategy.UploadLocal) }
                 ) { Text("Upload") }
+                DialogButton(hideErrorDialog) { Text("Cancel") }
             }
         }
     }
 
-    MultiplatformDialog(
+    ExperimentalMultiplatformDialog(
         onDismissRequest = {},
         title = { Text(text = "Sync") },
         content = dialogContent,
         buttons = dialogButtons
     )
 
+}
+
+@Composable
+private fun DialogButtonIcon(imageVector: ImageVector) {
+    Icon(
+        imageVector = imageVector,
+        contentDescription = null,
+        modifier = Modifier.size(20.dp)
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
+@Composable
+private fun FlowRowScope.DialogButton(
+    onClick: () -> Unit,
+    content: @Composable RowScope.() -> Unit
+) {
+    CompositionLocalProvider(
+        LocalTextStyle provides MaterialTheme.typography.labelLarge,
+        LocalContentColor provides MaterialTheme.colorScheme.surface,
+        LocalRippleConfiguration provides RippleConfiguration(
+            MaterialTheme.colorScheme.surface.copy(0.27f)
+        )
+    ) {
+
+        Row(
+            modifier = Modifier
+                .clip(MaterialTheme.shapes.medium)
+                .background(MaterialTheme.colorScheme.onSurface)
+                .clickable(onClick = onClick)
+                .padding(horizontal = 12.dp, vertical = 12.dp)
+                .fillMaxRowHeight(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            content = content
+        )
+
+    }
+}
+
+@Composable
+private fun LoadingLayout(
+    imageVector: ImageVector,
+    message: String
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Icon(
+            imageVector = imageVector,
+            contentDescription = null,
+            modifier = Modifier.size(60.dp),
+            tint = MaterialTheme.colorScheme.onSurface
+        )
+        androidx.compose.material.LinearProgressIndicator(
+            color = MaterialTheme.colorScheme.primary,
+            backgroundColor = MaterialTheme.colorScheme.surfaceVariant,
+            strokeCap = StrokeCap.Round
+        )
+        Text(message)
+    }
+}
+
+@Composable
+private fun MessageLayout(
+    title: String,
+    message: String,
+    imageVector: ImageVector = Icons.Outlined.CloudSync,
+    titleColor: Color = MaterialTheme.colorScheme.onSurface
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 16.dp)
+    ) {
+
+        Icon(
+            imageVector = imageVector,
+            contentDescription = null,
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+                .size(60.dp),
+            tint = MaterialTheme.colorScheme.onSurface
+        )
+
+        Text(
+            text = title,
+            style = MaterialTheme.typography.titleMedium,
+            color = titleColor,
+            modifier = Modifier.padding(bottom = 4.dp)
+        )
+
+        Text(
+            text = message,
+            style = MaterialTheme.typography.bodySmall,
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+        )
+    }
 }
