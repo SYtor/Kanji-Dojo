4cbb8e12fb55641fd21affa8562a303095c68adf
syt0r <shuliak.yaroslav@gmail.com>
2025-01-10 13:15:51 +0200

    Updated stats screen UI, calendar controls

--- core/src/commonMain/kotlin/ua/syt0r/kanji/presentation/screen/main/screen/home/screen/stats/use_case/SubscribeOnStatsDataUseCase.kt
+++ core/src/commonMain/kotlin/ua/syt0r/kanji/presentation/screen/main/screen/home/screen/stats/use_case/SubscribeOnStatsDataUseCase.kt
@@ -1,10 +1,18 @@
 package ua.syt0r.kanji.presentation.screen.main.screen.home.screen.stats.use_case
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.snapshotFlow
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.drop
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.datetime.DateTimeUnit
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.atStartOfDayIn
+import kotlinx.datetime.plus
 import kotlinx.datetime.toLocalDateTime
 import ua.syt0r.kanji.core.RefreshableData
 import ua.syt0r.kanji.core.logger.Logger
@@ -13,26 +21,20 @@ import ua.syt0r.kanji.core.srs.LetterPracticeType
 import ua.syt0r.kanji.core.srs.LetterSrsManager
 import ua.syt0r.kanji.core.srs.VocabPracticeType
 import ua.syt0r.kanji.core.time.TimeUtils
-import ua.syt0r.kanji.core.user_data.practice.ReviewHistoryItem
 import ua.syt0r.kanji.core.user_data.practice.ReviewHistoryRepository
 import ua.syt0r.kanji.presentation.LifecycleState
+import ua.syt0r.kanji.presentation.screen.main.screen.home.screen.stats.DayStats
+import ua.syt0r.kanji.presentation.screen.main.screen.home.screen.stats.RefreshableStats
+import ua.syt0r.kanji.presentation.screen.main.screen.home.screen.stats.StatsData
+import ua.syt0r.kanji.presentation.screen.main.screen.home.screen.stats.TimePeriodStats
+import ua.syt0r.kanji.presentation.screen.main.screen.home.screen.stats.TotalStats
+import ua.syt0r.kanji.presentation.screen.main.screen.home.screen.stats.YearMonth
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.minutes
 
 interface SubscribeOnStatsDataUseCase {
     operator fun invoke(lifecycleState: StateFlow<LifecycleState>): Flow<RefreshableData<StatsData>>
 }
-
-data class StatsData(
-    val today: LocalDate,
-    val yearlyPractices: Map<LocalDate, Int>,
-    val todayReviews: Int,
-    val todayTimeSpent: Duration,
-    val totalReviews: Int,
-    val totalTimeSpent: Duration,
-    val uniqueLettersStudied: Int,
-    val uniqueWordsStudied: Int
-)
 
 class DefaultSubscribeOnStatsDataUseCase(
     private val letterSrsManager: LetterSrsManager,
@@ -46,40 +48,90 @@ class DefaultSubscribeOnStatsDataUseCase(
         return refreshableDataFlow(
             dataChangeFlow = letterSrsManager.dataChangeFlow,
             lifecycleState = lifecycleState,
-            valueProvider = { getStats() }
+            valueProvider = { getStats(this) }
         )
     }
 
-    private suspend fun getStats(): StatsData {
+    private suspend fun getStats(coroutineScope: CoroutineScope): StatsData {
         Logger.logMethod()
         val today = timeUtils.getCurrentDate()
 
-        val timeZone = TimeZone.currentSystemDefault()
-        val yearStart = LocalDate(today.year, 1, 1).atStartOfDayIn(timeZone)
-        val yearEnd = LocalDate(today.year + 1, 1, 1).atStartOfDayIn(timeZone)
-
-        val yearlyReviewsToDateMap = reviewHistoryRepository.getReviews(yearStart, yearEnd)
-            .map { it to it.timestamp.toLocalDateTime(timeZone).date }
-
-        val dateToReviews: Map<LocalDate, List<ReviewHistoryItem>> = yearlyReviewsToDateMap
-            .groupBy { it.second }
-            .toList()
-            .associate { it.first to it.second.map { it.first } }
-
-        val todayReviews = dateToReviews[today] ?: emptyList()
-
         return StatsData(
-            today = today,
-            yearlyPractices = dateToReviews.mapValues { (_, practices) -> practices.size },
-            todayReviews = todayReviews.size,
-            todayTimeSpent = todayReviews.map { it.duration }
+            todayStats = getDayStats(today),
+            monthStats = refreshableStats(
+                coroutineScope = coroutineScope,
+                initialTimePeriod = YearMonth(today.year, today.month),
+                statsProvider = { getMonthStats(it) }
+            ),
+            yearStats = refreshableStats(
+                coroutineScope = coroutineScope,
+                initialTimePeriod = today.year,
+                statsProvider = { getYearStats(it) }
+            ),
+            totalStats = getTotalStats()
+        )
+    }
+
+    private suspend fun getDayStats(date: LocalDate): DayStats {
+        val stats = getStatsForPeriod(date, date.plus(1, DateTimeUnit.DAY))
+        val reviews = stats.dateToReviewsMap.entries.firstOrNull()?.value ?: emptyList()
+        return DayStats(
+            date = date,
+            reviews = reviews.size,
+            timeSpent = reviews
+                .map { it.duration }
                 .fold(Duration.ZERO) { acc, duration ->
                     acc.plus(duration.coerceAtMost(SingleReviewDurationLimit))
-                },
-            totalReviews = reviewHistoryRepository
+                }
+        )
+    }
+
+    private suspend fun <TimePeriod, Stats> refreshableStats(
+        coroutineScope: CoroutineScope,
+        initialTimePeriod: TimePeriod,
+        statsProvider: suspend (TimePeriod) -> Stats
+    ): RefreshableStats<TimePeriod, Stats> {
+
+        val timePeriodState = mutableStateOf(initialTimePeriod)
+        val statsState = mutableStateOf(statsProvider(initialTimePeriod))
+        val isLoading = mutableStateOf(false)
+
+        snapshotFlow { timePeriodState.value }
+            .drop(1)
+            .onEach {
+                isLoading.value = true
+                statsState.value = statsProvider(it)
+                isLoading.value = false
+            }
+            .launchIn(coroutineScope)
+
+        return RefreshableStats(
+            currentTimePeriod = initialTimePeriod,
+            selectedTimePeriodState = timePeriodState,
+            isLoading = isLoading,
+            statsState = statsState
+        )
+    }
+
+    private suspend fun getMonthStats(month: YearMonth): TimePeriodStats {
+        val monthStart = LocalDate(month.year, month.month, 1)
+        val monthEnd = monthStart.plus(1, DateTimeUnit.MONTH)
+        return getStatsForPeriod(monthStart, monthEnd)
+    }
+
+    private suspend fun getYearStats(year: Int): TimePeriodStats {
+        val yearStart = LocalDate(year, 1, 1)
+        val yearEnd = LocalDate(year + 1, 1, 1)
+        return getStatsForPeriod(yearStart, yearEnd)
+
+    }
+
+    private suspend fun getTotalStats(): TotalStats {
+        return TotalStats(
+            reviews = reviewHistoryRepository
                 .getTotalReviewsCount()
                 .toInt(),
-            totalTimeSpent = reviewHistoryRepository
+            timeSpent = reviewHistoryRepository
                 .getTotalPracticeTime(SingleReviewDurationLimit.inWholeMilliseconds),
             uniqueLettersStudied = reviewHistoryRepository
                 .getUniqueReviewItemsCount(LetterPracticeType.srsPracticeTypeValues)
@@ -87,6 +139,32 @@ class DefaultSubscribeOnStatsDataUseCase(
             uniqueWordsStudied = reviewHistoryRepository
                 .getUniqueReviewItemsCount(VocabPracticeType.srsPracticeTypeValues)
                 .toInt()
+        )
+    }
+
+    private suspend fun getStatsForPeriod(
+        start: LocalDate,
+        end: LocalDate
+    ): TimePeriodStats {
+        val timeZone = TimeZone.currentSystemDefault()
+        val reviewsToDateMap = reviewHistoryRepository
+            .getReviews(
+                start.atStartOfDayIn(timeZone),
+                end.atStartOfDayIn(timeZone)
+            )
+            .map { historyItem ->
+                historyItem to historyItem.timestamp.toLocalDateTime(timeZone).date
+            }
+
+        val dateToReviewsMap = reviewsToDateMap
+            .groupBy { (_, date) -> date }
+            .toList()
+            .associate { (date, items) ->
+                date to items.map { (reviewHistoryItem, date) -> reviewHistoryItem }
+            }
+
+        return TimePeriodStats(
+            dateToReviewsMap = dateToReviewsMap.mapValues { (_, practices) -> practices },
         )
     }
 
