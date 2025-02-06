3b62a61865f7e88608001efcd8fc4e7fff29b3b6
dreamyuki <49896401+dreamyukii@users.noreply.github.com>
2025-02-06 17:08:46 +0700

    Flathub support, linux builds refactoring
    Co-authored-by: syt0r <shuliak.yaroslav@gmail.com>

---
+++ app/linux/flatpak/kanji-dojo.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+EXECUTABLE="$APP_DIR/app/bin/Kanji Dojo"
+exec "$EXECUTABLE" "$@"
--- core/src/jvmMain/kotlin/ua/syt0r/kanji/presentation/common/ComposeUrlJvm.kt
+++ core/src/jvmMain/kotlin/ua/syt0r/kanji/presentation/common/ComposeUrlJvm.kt
@@ -2,16 +2,13 @@ package ua.syt0r.kanji.presentation.common
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import java.awt.Desktop
-import java.net.URI
 
 @Composable
 actual fun rememberUrlHandler(): UrlHandler {
     return remember {
         object : UrlHandler {
             override fun openInBrowser(url: String) {
-                // TODO handle if Desktop.isDesktopSupported() is false (no gnome on linux)
-                Desktop.getDesktop().browse(URI.create(url))
+                ProcessBuilder("xdg-open", url).start()
             }
         }
     }
